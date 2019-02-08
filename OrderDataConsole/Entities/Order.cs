92b4f79cc193e2f54532a822e5f187a38c258543
Regis Lima <regislima1391@gmail.com>
2019-02-08 14:10:51 -0300

    Adicionada a classe Order com seus construtores

---
+++ OrderDataConsole/Entities/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using OrderDataConsole.Entities.Enums;
+
+namespace OrderDataConsole.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; } = DateTime.Now;
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+        }
+
+        public Order(OrderStatus status, Client client)
+        {
+            Status = status;
+            Client = client;
+        }
+
+
+    }
+}
