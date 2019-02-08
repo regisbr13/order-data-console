70bb4b5b7fd715ed3e071cc67b195d70b8680bfa
Regis Lima <regislima1391@gmail.com>
2019-02-08 14:01:21 -0300

    Criada a entidade OrderItem, seus construtores e o metodo SubTotal

---
+++ OrderDataConsole/Entities/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿namespace OrderDataConsole.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public Product Product { get; set; } = new Product();
+
+        public OrderItem()
+        {
+        }
+
+        public OrderItem(int quantity, Product product)
+        {
+            Quantity = quantity;
+            Product = product;
+        }
+
+        public double SubTotal()
+        {
+            return Quantity * Product.Prince;
+        }
+    }
+}
